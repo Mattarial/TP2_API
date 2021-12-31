7520d81a7ab0136fda6f4ee8709d7275cd61d3bc
Mattarial <77466192+Mattarial@users.noreply.github.com>
2021-12-31 15:23:08 +0100

    Delete tp5 directory

--- tp5/src/main/java/fr/ensim/tp5/model/AddressRepository.java
+++
@@ -1,9 +0,0 @@
-package fr.ensim.tp5.model;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AddressRepository extends CrudRepository<Address, Long> {
-
-}
